ecb498fd60b69db25b6472e9365e27233a85ed45
jayamrutiya <amrutiyajay98@gmail.com>
2023-09-17 18:42:35 +0530

    change migration file.

--- prisma/migrations/20230909070614_0/migration.sql
+++ prisma/migrations/20230909070614_0/migration.sql
@@ -1,21 +1,3 @@
--- -- DropIndex
--- DROP INDEX `Customer_customerMasterId_fkey` ON `customer`;
-
--- -- DropIndex
--- DROP INDEX `Customer_userId_fkey` ON `customer`;
-
--- -- DropIndex
--- DROP INDEX `CustomerMaster_userId_fkey` ON `customermaster`;
-
--- -- DropIndex
--- DROP INDEX `CustomerTemplate_customerId_fkey` ON `customertemplate`;
-
--- -- DropIndex
--- DROP INDEX `CustomerTemplate_templateId_fkey` ON `customertemplate`;
-
--- -- DropIndex
--- DROP INDEX `Template_userId_fkey` ON `template`;
-
 -- AlterTable
 ALTER TABLE `CustomerTemplate` ADD COLUMN `customerTemplateMasterId` INTEGER NULL;
 
@@ -35,17 +17,8 @@ CREATE TABLE `CustomerTemplateMaster` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- -- AddForeignKey
--- ALTER TABLE `Template` ADD CONSTRAINT `Template_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `CustomerMaster` ADD CONSTRAINT `CustomerMaster_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- -- AddForeignKey
--- ALTER TABLE `Customer` ADD CONSTRAINT `Customer_customerMasterId_fkey` FOREIGN KEY (`customerMasterId`) REFERENCES `CustomerMaster`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- -- AddForeignKey
--- ALTER TABLE `Customer` ADD CONSTRAINT `Customer_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `CustomerTemplateMaster` ADD CONSTRAINT `CustomerTemplateMaster_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -55,9 +28,3 @@ ALTER TABLE `CustomerTemplateMaster` ADD CONSTRAINT `CustomerTemplateMaster_cust
 
 -- AddForeignKey
 ALTER TABLE `CustomerTemplate` ADD CONSTRAINT `CustomerTemplate_customerTemplateMasterId_fkey` FOREIGN KEY (`customerTemplateMasterId`) REFERENCES `CustomerTemplateMaster`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- -- AddForeignKey
--- ALTER TABLE `CustomerTemplate` ADD CONSTRAINT `CustomerTemplate_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- -- AddForeignKey
--- ALTER TABLE `CustomerTemplate` ADD CONSTRAINT `CustomerTemplate_templateId_fkey` FOREIGN KEY (`templateId`) REFERENCES `Template`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
